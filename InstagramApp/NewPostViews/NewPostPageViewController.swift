67adda1e45dbe28fcc678c4fdd8864dbf15e12f3
abhishekbiswas772 <abhishekbiswas987@google.com>
2023-04-13 18:27:02 +0530

    changes in newPostPageViewController added pageViewController delegate methods

--- InstagramApp/NewPostViews/NewPostPageViewController.swift
+++ InstagramApp/NewPostViews/NewPostPageViewController.swift
@@ -8,13 +8,70 @@
 
 import UIKit
 
-class NewPostPageViewController: UIPageViewController {
+class NewPostPageViewController: UIPageViewController, UIPageViewControllerDelegate {
+    
+    var orderedController : [UIViewController] = []
+    var currentViewControllerIndex : Int = 0
+    var pagesToBeShown : [NewPageConstants.newPageToShow] = NewPageConstants.newPageToShow.pageToBeingShown()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        self.delegate = self
+        self.dataSource = self
+        preparePageViewControllerBeforeStarting()
     }
     
+    func preparePageViewControllerBeforeStarting(){
+        for pageType in self.pagesToBeShown{
+            let pageViewController = prepareCurrentViewController(pageType)
+            self.orderedController.append(pageViewController)
+        }
+        
+        if let firstStartViewController = self.orderedController.first {
+            setViewControllers([firstStartViewController], direction: .forward, animated: true)
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(newPostNotificationAction(_ :)), name: NSNotification.Name("newPostsAction"), object: nil)
+    }
+    
+    
+    @objc func newPostNotificationAction(_ notification: Notification){
+        if let receviedNotifedObject = notification.object as? NewPageConstants.newPageToShow {
+            presentCurrentControllerAfterPrepare(receviedNotifedObject.rawValue)
+        }
+    }
+    
+    func prepareCurrentViewController(_ pagesTobeSeen: NewPageConstants.newPageToShow) -> UIViewController {
+        var currentViewController : UIViewController?
+        switch pagesTobeSeen {
+        case .camera:
+            currentViewController = UIStoryboard(name: Constants.newPostStoryBoardName, bundle: nil).instantiateViewController(withIdentifier: Constants.CameraVCID) as? CameraViewController
+            break
+        case .library:
+            currentViewController = UIStoryboard(name: Constants.newPostStoryBoardName, bundle: nil).instantiateViewController(withIdentifier: Constants.LibraryVCID) as? LibraryViewController
+            break
+        }
+        return UIViewController()
+    }
+    
+    func presentCurrentControllerAfterPrepare(_ index: Int){
+        if self.currentViewControllerIndex > index {
+            self.setViewControllers([orderedController[index]], direction: .reverse, animated: true)
+        }else if self.currentViewControllerIndex < index {
+            setViewControllers([orderedController[index]], direction: .forward, animated: true)
+        }
+        
+        self.currentViewControllerIndex = index
+    }
+}
 
+extension NewPostPageViewController : UIPageViewControllerDataSource {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        <#code#>
+    }
 }
